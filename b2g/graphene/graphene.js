4e02dddc13a0e0347d33e85e8309921d783de52c
Kevin Grandon <kevingrandon@yahoo.com>
2015-03-19 17:22:47 -0700

    Adjust prefs for HIRO.

--- b2g/graphene/graphene.js
+++ b2g/graphene/graphene.js
@@ -2,7 +2,10 @@
 // for keyCode values.
 // Default value is F5
 pref("b2g.reload_key", '{ "key": 116, "shift": false, "ctrl": false, "alt": false, "meta": false }');
-pref("b2g.default.start_manifest_url", "https://mozilla.github.io/browser.html/manifest.webapp");
+pref("b2g.default.start_manifest_url", "http://localhost:8080/manifest.webapp");
+
+pref("dom.ipc.tabs.disabled", false);
+pref("dom.vr.enabled", true);
 
 pref("javascript.options.discardSystemSource", false);
 pref("dom.ipc.tabs.disabled", false);
